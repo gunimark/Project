fbf56554c1b595a609b0efd00c88d1fbee926fae
gunimark <markboonnak@gmail.com>
2025-11-24 03:17:31 +0700

    add database test

--- testdata.sql
+++ testdata.sql
@@ -1,5 +1,8 @@
 CREATE TABLE users(
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50),
+  name VARCHAR(50),
   password VARCHAR(255)
 );
+
+INSERT INTO users (name, password) VALUES
+('Mark', '1512');
